c71844b1e385e72e9513ae867158206aa82a7de2
Jessica D <dionisiojessica15@gmail.com>
2026-02-08 00:40:40 +0800

    fix: update logo path

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,23 +10,13 @@ function App() {
           justifyContent: "center",
         }}
       >
-        <img src="src/assets/day6_zip.png" style={{ width: "300px" }} />
+        <img src="/src/assets/day6_zip.png" style={{ width: "300px" }} />
       </Box>
       <Grid
         container
         spacing={2}
         sx={{ display: "flex", justifyContent: "center" }}
       >
-        {/* <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/videoseries?si=ZKGZsGMIz7T03w8N&amp;list=PLI6-3D3hE5xilbhU4Wvdl49zm7tRIxkTL"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerpolicy="strict-origin-when-cross-origin"
-          allowfullscreen
-        ></iframe> */}
         <YoutubePlayer />
       </Grid>
     </>
